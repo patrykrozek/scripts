94f5cffd6a8461435d86834501ae2d65f148b9cc
Rozek <vboxuser@Xubuntu.myguest.virtualbox.org>
2022-12-02 13:42:07 +0100

    This is my starter script which helps me to make my project faster.

---
+++ newproject.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+mkdir bin
+mkdir include
+mkdir src
+touch .gitignore
+echo -e ".vscode/\nbin/" >> .gitignore
+
+
+
+touch run.sh
+cat <<- 'EOF' > run.sh
+#!/bin/bash
+rm ./bin/main
+g++ ./src/main.cpp -o ./bin/main &&  
+./bin/main
+EOF 
+sudo chmod +x ./run.sh
+
+
+touch ./src/main.cpp
+cat <<- 'EOF' > ./src/main.cpp
+#include <iostream>
+int main ()
+{
+
+}
+EOF
